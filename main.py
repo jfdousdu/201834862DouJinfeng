ad7c6412320674c99313f2e835dd2687f9ba00f9
Donna <jinfengdou@mail.sdu.edu.cn>
2018-11-03 22:52:18 +0800

    main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-from  201834862DouJinfeng import knn,data_process as dp
-from sklearn.model_selection import  train_test_split
+import knn,data_process as dp
+from sklearn.model_selection import train_test_split
 
 FREQUENCY=15
 
@@ -20,10 +20,10 @@ def tf_idf(word_tf_test,word_doc_tf_train,word_idf_train,num_doc_train,doc_word_
             tf = doc_word_tf_train[index].get(word,0)
             if(tf!=0):
                 tf = 1+ math.log(tf)
-             tmp.append(tf*word_idf_train[word])
+            tmp.append(tf*word_idf_train[word])
         x_train.append(tmp)
         index_train.append(index)
-     return  x_test,x_train,x_train
+    return  x_test,x_train,x_train
 
 #返回准确率
 def compute_acc():
@@ -46,10 +46,11 @@ def compute_acc():
         if(y_eval ==Y_test[i]):
             num_right= num_right+1
         index += 1
-        if(index%10==0)
+        if(index%10==0):
             print(index,' ',num_right/i)
     return num_right/num_test
 
+#读取文件，保存这些数据
 def data_save():
      docs_list,labels_list = dp.doc_load()
      words_list = dp.doc_split(docs_list)
@@ -71,18 +72,44 @@ def data_save():
      with open('.\\tmp\\Y_test.txt','w') as data:
          data.write(str(Y_test))
 
-
+#计算准确率
 def data_load():
-    with open('.\\tmp\\word_doc_tf_train.txt', 'w') as data:
+    with open('.\\tmp\\word_doc_tf_train.txt', 'r') as data:
         tmp = data.read()
         word_doc_tf_train = eval(tmp)
-    with open('.\\tmp\\word_idf_train.txt', 'w') as data:
-        data.write(str(word_idf_train))
-    with open('.\\tmp\\doc_word_tf_train.txt', 'w') as data:
-        data.write(str(doc_word_tf_train))
-    with open('.\\tmp\\doc_word_tf_test.txt', 'w') as data:
-        data.write(str(doc_word_tf_test))
-    with open('.\\tmp\\Y_train.txt', 'w') as data:
-        data.write(str(Y_train))
+    with open('.\\tmp\\word_idf_train.txt', 'r') as data:
+        tmp = data.read()
+        word_idf_train = eval(tmp)
+    with open('.\\tmp\\doc_word_tf_train.txt', 'r') as data:
+        tmp = data.read()
+        doc_word_tf_train = eval(tmp)
+    with open('.\\tmp\\doc_word_tf_test.txt', 'r') as data:
+        tmp = data.read()
+        doc_word_tf_test = eval(tmp)
+    with open('.\\tmp\\Y_train.txt', 'r') as data:
+        tmp = data.read()
+        Y_train = eval(tmp)
     with open('.\\tmp\\Y_test.txt', 'w') as data:
-        data.write(str(Y_test))
+        tmp = data.read()
+        Y_test = eval(tmp)
+    return word_doc_tf_train,word_idf_train,doc_word_tf_train,doc_word_tf_test,Y_train,Y_test
+
+
+def compute_acc_without_reload():
+     index = 0
+     word_doc_tf_train,word_idf_train,doc_word_tf_train.doc_word_tf_test,Y_train,Y_test=data_load()
+
+     num_test = len(doc_word_tf_test)
+     num_docs_train = len(doc_word_tf_train)
+     num_right = 0
+     print('Prepare already Finished!')
+     for i in range(num_test):
+         x_test,x_train,index_train = tf_idf(doc_word_tf_test[i],word_doc_tf_train,word_idf_train,num_docs_train,word_doc_tf_train)
+         y_train = [Y_train[j] for j in index_train]
+         y_eval = knn.knn_cal(x_test,x_train,y_train,5)
+         if(y_eval == Y_test[i]):
+             num_right = num_right+1
+         index += 1
+         if(index%10==0):
+             print(index,' ',num_right/i)
+     return num_right/num_test
