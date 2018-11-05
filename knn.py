786419eab8004754fa9a42ab61ceba50cbb28266
Donna <jinfengdou@mail.sdu.edu.cn>
2018-11-05 21:00:54 +0800

    main

--- knn.py
+++ knn.py
@@ -1,6 +1,6 @@
 import numpy as np
 import operator
-
+'''
 #分别计算x_test和x_train的cos值，用于比较两者的相似性，如果越接近1则表明结果越相近
 def cos_compute(x_test,x_train):
     len_test  = (x_test**2).sum()**0.5
@@ -11,6 +11,7 @@ def cos_compute(x_test,x_train):
         cos.append(np.dot(x_test,x_train[i]))
     cos = cos/(len_test*len_train)
     return cos
+'''
 
 #计算欧几里得距离，用于比较相似，其值越小越相似
 def euclidean_compute(x_test,x_train):
@@ -34,4 +35,3 @@ def knn_cal(x_test,x_train,y_train,k):
         classCount[voteIlabel]=classCount.get(voteIlabel,0)+1
     sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]
-
