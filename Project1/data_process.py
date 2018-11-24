15da21a07e946ce2a7d4932bfcd439d06a6d01b0
Donna <jinfengdou@mail.sdu.edu.cn>
2018-11-24 22:58:50 +0800

    实验二

--- Project1/data_process.py
+++ Project1/data_process.py
@@ -9,15 +9,15 @@ import nltk
 
 #读取文档字典
 def doc_load():
-    dirs = os.listdir("C:\\Users\\Donna\\PycharmProjects\\201834862DouJinfeng\\20news-18828") #列出所在文件夹中的所有文件夹的名字，返回一个文件夹名组成的数组
+    dirs = os.listdir("C:\\Users\\Donna\\PycharmProjects\\201834862DouJinfeng\\Project1\\20news-18828") #列出所在文件夹中的所有文件夹的名字，返回一个文件夹名组成的数组
     docs_list = [] #创建一个空数组,存储文档
     labels_list = [] #创建一个空数组,存储文档类，别相当于标签
     label = 0  #列出labeles_lists数组的值从0开始
 
     for dir in dirs : #循环，循环遍历每一个文件夹，新生成的dir为一个标记dirs的变量
-         docs = os.listdir("C:\\Users\\Donna\\PycharmProjects\\201834862DouJinfeng\\20news-18828"+'\\'+dir)
+         docs = os.listdir("C:\\Users\\Donna\\PycharmProjects\\201834862DouJinfeng\\Project1\\20news-18828"+'\\'+dir)
          for doc in docs :
-             with open ("C:\\Users\\Donna\\PycharmProjects\\201834862DouJinfeng\\20news-18828"+'\\'+dir+'\\'+doc,'rb') as data:  #打开文档
+             with open ("C:\\Users\\Donna\\PycharmProjects\\201834862DouJinfeng\\Project1\\20news-18828"+'\\'+dir+'\\'+doc,'rb') as data:  #打开文档
                  docs_list.append(data.read()) #取读文档
              labels_list.append(label)
 
