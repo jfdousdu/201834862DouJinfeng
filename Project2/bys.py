a3ba3bf004eba5b7c73dfa6fed29f1f1692e75d9
Donna <jinfengdou@mail.sdu.edu.cn>
2018-11-25 22:39:19 +0800

    实验二

--- Project2/bys.py
+++ Project2/bys.py
@@ -4,138 +4,42 @@ import numpy as np
 import math
 NUM_CLASS=20# 类的个数
 
-def get_vocab(X_train):
-    '''
-    从单词数组中，得到相应词汇表
-    :param X_train: list, words
-    :return: vocab,list, 词汇表 vocabulary
-    '''
-    vocab=set()
-    for doc in X_train:
-        vocab=vocab|set(doc)
-    return list(vocab)
-
-def words_freq_proc(words_list,frequency):
-    '''
-    去掉在words_list中出现次数小于frequency的单词，去掉每个文档中的重复单词
-    :param words_list: list, words
-    :param frequency:
-    :return:new_wrods_list，其中没有词频小于frequency的单词，而且每一个文档中的每一个单词只出现一次
-    '''
-    word_frequency=dict()
-    for doc in words_list:
-        for word in doc:
-            word_frequency[word]=word_frequency.get(word,0)+1
-    new_words_list=[]
-    for doc in words_list:
-        new_list=set()
-        for word in doc:
-            if(word_frequency[word]>=frequency):
-                new_list.add(word)
-        new_words_list.append(list(new_list))
-    return new_words_list
-
-def naive_bayes(x_test,y_x_prob,y_prob,vocab):
-    '''
-    伯努利类型朴素贝叶斯分类
-    :param x_test: list
-    :param y_x_prob: list P(x|y)
-    :param y_prob: list P(y)
-    :param vocab: list
-    :return:
-    '''
-    res_prob=[0]*NUM_CLASS
+'''
+the vocabulary of train 
+'''
+def calc_x_y_prob(x_train,y_train):
+    res = [{} for i  in range(NUM_CLASS)] #########
+    num_all = [0]*NUM_CLASS
+    num_docs = len(x_train)
+    for i in range(num_docs):
+        for word in x_train[i]:
+            res[y_train[i]][word] = (res[y_train[i]].get(word,0)+1)
+            num_all[y_train[i]]+=1
     for i in range(NUM_CLASS):
-        for j in range(len(vocab)):
-            if(x_test.__contains__(vocab[j])):
-                res_prob[i]+=math.log(y_x_prob[i][j])
-            else:
-                res_prob[i]+=math.log(1-y_x_prob[i][j])
-        res_prob[i]+=math.log(y_prob[i])
-    return np.array(res_prob).argmax()
+        for key in res[i]:
+            res[i][key] = (res[i][key]+1)/(num_all[i][key]+len(list(res[i].keys())))
+    return res,num_all
 
 def calc_y_prob(Y_train):
-    '''
-    计算P(y)
-    :param Y_train:
-    :return:
-    '''
-    y_prob=[1]*NUM_CLASS
+    y_prob = [1]*NUM_CLASS
     for label in Y_train:
         y_prob[label]+=1
     for i in range(NUM_CLASS):
-        y_prob[i]=y_prob[i]/(len(Y_train)+NUM_CLASS)
+        y_prob[i] = y_prob[i]/(len(Y_train)+NUM_CLASS)
     return y_prob
 
+def naive_bayes(x_test,x_y_prob,y_prob,num_all):
+    res_prob = [0]*NUM_CLASS
+    for i in range(NUM_CLASS):
+        for word in x_test:
+            tmp_prob = x_y_prob[i].get(word,0)
+            if(tmp_prob!=0):
+                res_prob[i] += math.log(tmp_prob)
+            else:
+                res_prob[i] += math.log(1/(num_all[i]+len(list(x_y_prob[i].keys))))
+        res_prob[i]+=math.log(y_prob[i])
+    return np.array(res_prob).argmax()
 
-def main():
-    '''
-    计算预测准确率
-    :param
-    :return:
-    '''
-    X_train, Y_train, X_test, Y_test = data_load()
-    vocab=get_vocab(X_train)
-    y_prob=calc_y_prob(Y_train)#计算P(y)
-    #y_x_num=[[1]*len(vocab)]*NUM_CLASS#取1的概率   坑爹呀! 二维数组不能这样初始化，如果这样生成二维数组，其每一个元素其实都是指向的同一个位置y_xnum[0]和y_num[1]是指向的同一个位置
-    y_x_num=[[1]*len(vocab) for i in range(NUM_CLASS)]#
-    y_num=[2]*NUM_CLASS#每个类对应的文档总个数\
-    for i in range(len(X_train)):#计算y_x_num,和y_num，用于计算P(xi|y)
-        for word in X_train[i]:
-            y_x_num[Y_train[i]][vocab.index(word)]+=1# 相应的class里的相应的单词的个数加1
-        y_num[Y_train[i]]+=1
-
-    y_x_prob=[]###相应特征取1的概率
-    for i in range(NUM_CLASS):#求得概率
-        y_x_prob.append(list(np.array(y_x_num[i])/y_num[i]))
-    #predict X_test and calculate accuracy of prediction.
-    num_right=0
-    for i in range(len(X_test)):
-        label=naive_bayes(X_test[i],y_x_prob,y_prob,vocab)
-        if(label==Y_test[i]):
-            num_right+=1
-        if((i+1)%10==0):
-            print(num_right/(i+1))
-    return num_right/len(X_test)
-
-
-def data_load():
-    '''
-    读取中间数据:X_train,Y_train,X_test,Y_test
-    :return:
-    '''
-    with open('.\\tmp\\X_train.txt','r') as data:
-        tmp=data.read()
-        X_train=eval(tmp)
-    with open('.\\tmp\\Y_train.txt','r') as data:
-        tmp=data.read()
-        Y_train=eval(tmp)
-    with open('.\\tmp\\X_test.txt','r') as data:
-        tmp=data.read()
-        X_test=eval(tmp)
-    with open('.\\tmp\\Y_test.txt','r') as data:
-        tmp=data.read()
-        Y_test=eval(tmp)
-    return X_train,Y_train,X_test,Y_test
-
-def data_save():
-    '''
-    保存中间数据:X_train,Y_train,X_test,Y_test
-    :return:
-    '''
-    docs_list, labels_list = dm.doc_load()
-    words_list = dm.doc_split(docs_list)
-    words_list1 = words_freq_proc(words_list, 15)
-    X_train, X_test, Y_train, Y_test = train_test_split(words_list1, labels_list, test_size=0.2, random_state=42)
-
-    with open('.\\tmp\\X_train.txt','w') as data:
-        data.write(str(X_train))
-    with open('.\\tmp\\Y_train.txt','w') as data:
-        data.write(str(Y_train))
-    with open('.\\tmp\\X_test.txt','w') as data:
-        data.write(str(X_test))
-    with open('.\\tmp\\Y_test.txt','w') as data:
-        data.write(str(Y_test))
 
 if __name__=="__main__":
     print("begin")
--- Project2/dp.py
+++ Project2/dp.py
@@ -1,127 +1,102 @@
-
 import os
 import re
 import textblob as tb
-from nltk.corpus import stopwords as pw
 import math
-
+from nltk.corpus import stopwords as pw
 
 ##the path of the data article
 newsPath = 'C:\\Users\\Donna\\PycharmProjects\\201834862DouJinfeng\\Project1\\20news-18828'
 
+'''
+docs_list,labels_list  to discovery these article and labels
+'''
 def doc_load():
-    '''
-    :return:文档字典
-    '''
-    dirs = os.listdir(newsPath)#列出newPath所在文件夹中的所有文件夹的名字，返回一个文件夹名组成的数组
-    docs_list=[]#创建一个空数组,存储文档
-    labels_list=[]#存储文档类别
-    label=0#类别
-    for dir in dirs:#循环，遍历所有文件夹
-        docs=os.listdir(newsPath+'\\'+dir)#文件夹的路径
-        for doc in docs:#遍历文件夹中的所有文档
-            with open(newsPath+'\\'+dir+ '\\' + doc,'rb') as data:#打开文档
-                docs_list.append(data.read())#读出文档
+    dirs =  os.listdir(newsPath)
+    docs_list = []
+    labels_list = []
+    label = 0
+    for dir in dirs:
+        docs = os.listdir(newsPath+'\\'+dir)
+        for doc in docs:
+            with open(newsPath+'\\'+dir+'\\'+doc,'rb') as data:
+                docs_list.append(data.read())
             labels_list.append(label)
-        label=label+1
+        label = label+1
     return docs_list,labels_list
 
-
-
+'''
+cut the str into words
+'''
 def _string_split(str):
-    '''
-    internal
-    :param str:
-    :return: 把str分割成的单词数组
-    '''
-    #tb=TextBlob(bigString)
-    #words=tb.words.singularize().lemmatize().lower()##
-    #bigString=TextBlob(bigString)
-    #bigString.correct()
-    str=str.lower()
-    #words=nltk.wordpunct_tokenize(bigString)
-    words=re.split('[^a-z]*',str)## 以所有除字母以外的符号作为分隔符进行分词
-    words = [word for word in words if len(word) >= 3]  ##去掉长度小于三的词
-    words=tb.WordList(words).singularize()#复数变单数
-    words=[word.lemmatize('v') for word in words]#过去式、进行时变一般形式
-    cacheStopWords=pw.words("english")#得到stopwords
-    words=[word for word in words if word not in cacheStopWords]## remove stopwords
+    str = str.lower()
+    words = re.split('[^a-z]*',str)  ##split into words
+    words = [word for word in words if len(word)>=3]
+    words = tb.WordList(words).singularize()
+    words = [word.lemmatize('v') for word in words]
+    catcheStopWords = pw.words("english")
+    words = [word for word in words if word not in catcheStopWords]
     return words
 
+'''
+cut docs_list into doc_list 
+'''
 def doc_split(docs_list):
-    '''
-    :param :docs_list: 文档字典
-    :return words_list
-    '''
-    words_list=[]
+    words_list = []
     for doc in docs_list:
         words_list.append(_string_split(str(doc)))
     return words_list
+
+'''
+built vocabulary dictionary
+'''
 def get_vocab(words_list):
-    vocab=set()
+    vocab = set()
     for doc in words_list:
-        vocab=vocab|set(doc)
+        vocab = vocab|set(doc)   ##Let's take the union of the two
     return list(vocab)
 
+
 def words_freq_proc(words_list,frequency):
-    '''
-    去掉单词在文档中出现频率低于FREQUENCY的词，并返回去掉低频词的words_list
-    :param words_list:
-    :return:
-    '''
-    word_frequency=dict()
-    #统计词频
+    word_frequency = dict()
+
     for doc in words_list:
         for word in doc:
-            word_frequency[word]=word_frequency.get(word,0)+1
-    new_words_list=[]
+            word_frequency[word] = word_frequency.get(word,0)+1
+    new_words_list = []
     for doc in words_list:
-        new_list=[]
+        new_list = []
         for word in doc:
             if(word_frequency[word]>=frequency):
                 new_list.append(word)
         new_words_list.append(new_list)
     return new_words_list
 
+################################################
 def words_statistics(words_list):
-    '''
-    :param words_dict:
-    :return:vocab:检测词频，创建词库
-    '''
-    index=0
-    #word_frequency=dict()# 单词在所有文档中出现的频率
-    word_df=dict()   #每个单词的df
-    word_doc_tf=dict()#每个单词在哪些文档中出现过
-    doc_word_tf=[]#  #每个文档中都包含哪些单词
+    index = 0
+    word_df = dict()
+    word_doc_tf = dict()
+    doc_word_tf = []
 
-    #统计word_doc_tf,doc_word_tf
     for doc in words_list:
-        #思想出现了问题，根本不行
-        #first_time = True  # 判断是否是在doc中第一次出现，用于统计df
-        word_tf=dict()
+        word_tf = dict()
         for word in doc:
-            word_tf[word]=word_tf.get(word,0)+1
-            #word_frequency[word] = word_frequency.get(word, 0) + 1### word_frequency
-            #错了，
-            # if(first_time):###word_df
-            #     word_df[word]=word_df.get(word,0)+1
-            #     first_time=False
-            if(word_doc_tf.__contains__(word)):#word_doc_tf
-                word_doc_tf[word][words_list.index(doc)]=word_doc_tf[word].get(words_list.index(doc),0)+1
+            word_tf[word] = word_tf.get(word,0)+1
+            if(word_doc_tf.__contains__(word)):
+                word_doc_tf[word][words_list.index(doc)] = word_doc_tf[word].get(words_list.index(doc),0)+1
             else:
-                word_doc_tf[word]={words_list.index(doc):1}
-        doc_word_tf.append(word_tf)
+                word_doc_tf[word] = {words_list.index(doc):1}
+    doc_word_tf.append(word_tf)
 
-    #统计word_df
+
     for doc in doc_word_tf:
         for word in doc.keys():
-            word_df[word]=word_df.get(word,0)+1
+            word_df[word] = word_df.get(word,0)+1
 
-    #通过word_df计算word_idf
-    num_docs=len(words_list)
-    word_idf=dict()
+
+    num_docs = len(words_list)
+    word_idf = dict()
     for word in word_df:
-        word_idf[word]=math.log(num_docs/word_df[word])
+        word_idf[word] = math.log(num_docs/word_df[word])
 
-
-    return word_doc_tf,word_idf,doc_word_tf
+    return word_doc_tf, word_idf, doc_word_tf
